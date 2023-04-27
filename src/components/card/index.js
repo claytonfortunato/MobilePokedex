e314c676a0f32beb0566feaa70a0c73f092a2f1a
claytonfortunato <claytonreis10@gmail.com>
2023-04-26 23:42:54 -0300

    feat: style card

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,13 +3,19 @@ import { View, StyleSheet, Image, Text } from "react-native";
 export const Card = ({ data }) => {
   return (
     <View style={styles.container}>
-      <Image
-        source={{
-          uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
-        }}
-      />
-      <Text style={styles.name}>{data.name}</Text>
-      <Text>#{data.id}</Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.number}>#{data.id}</Text>
+        <Text style={styles.name}>{data.name}</Text>
+      </View>
+
+      <View>
+        <Image
+          style={styles.image}
+          source={{
+            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -17,11 +23,33 @@ export const Card = ({ data }) => {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: 100,
-    backgroundColor: "#eaeaea",
+    height: 120,
+    backgroundColor: "#222",
     margin: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 80,
+  },
+  wrapper: {
+    flexDirection: "column",
+  },
+  number: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff",
   },
   name: {
-    fontSize: 16,
+    fontSize: 25,
+    fontWeight: "bold",
+    lineHeight: 31,
+    textTransform: "capitalize",
+    color: "#fff",
+  },
+  image: {
+    marginTop: -40,
+    width: 130,
+    height: 130,
   },
 });
